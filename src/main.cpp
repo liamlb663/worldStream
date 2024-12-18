2f6c8b9b27587c121356b0aee4a4c3df3d63c8ee
Liam Ahearn <liam663lb@gmail.com>
2024-12-18 05:53:06 -0500

    Change third_party usage + vulkan dep check

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 #include <GLFW/glfw3.h>
 #include <imgui.h>
 #include <glm/glm.hpp>
+#include <vulkan/vulkan.h>
 
 int main() {
     fmt::print("Hello from fmt!\n");
     spdlog::info("Hello from spdlog!");
 
+    // Initialize GLFW
     if (!glfwInit()) {
         spdlog::error("Failed to initialize GLFW!");
         return -1;
@@ -15,6 +17,52 @@ int main() {
 
     spdlog::info("GLFW initialized successfully!");
 
+    // Set up GLFW for Vulkan
+    if (!glfwVulkanSupported()) {
+        spdlog::error("Vulkan is not supported on this system!");
+        glfwTerminate();
+        return -1;
+    }
+
+    spdlog::info("Vulkan is supported!");
+
+    // Vulkan instance creation
+    VkInstance instance;
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+    // Check validation layers
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+    spdlog::info("Available validation layers: {}", layerCount);
+
+    // Application info
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Vulkan Test App";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    createInfo.pApplicationInfo = &appInfo;
+
+    // Create Vulkan instance
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        spdlog::error("Failed to create Vulkan instance!");
+        glfwTerminate();
+        return -1;
+    }
+
+    spdlog::info("Vulkan instance created successfully!");
+
+    // Clean up Vulkan
+    vkDestroyInstance(instance, nullptr);
+
+    // Terminate GLFW
     glfwTerminate();
+    spdlog::info("GLFW terminated successfully!");
+
     return 0;
 }
+
