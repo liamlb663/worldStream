dcea8b8033098f01433635312dc93fc9e5cf3c2c
Liam Ahearn <liam663lb@gmail.com>
2025-01-24 00:45:07 -0500

    Start framing the render object interface

---
+++ src/ResourceManagement/RenderResources/RenderObjects/RenderObject.hpp
@@ -0,0 +1,29 @@
+// src/ResourceManagement/RenderResources/RenderObjects/RenderObject.hpp
+
+#pragma once
+
+#include "Core/Types.hpp"
+#include "../Buffer.hpp"
+
+#include <glm/glm.hpp>
+#include <strings.h>
+#include <vector>
+
+struct Vertex {
+    glm::vec3 position;
+    F32 uv_x;
+    glm::vec3 normal;
+    F32 uv_y;
+};
+
+struct RenderObject {
+    U32 indexCount;
+    U32 startIndex;
+    Buffer* indexBuffer;
+
+};
+
+class IRenderable {
+public:
+    virtual std::vector<RenderObject> draw(const glm::mat4 parentMatrix) = 0;
+};
